b8b6ea8f916bfa1fdb592f34409e980bac667443
Ewald Zietsman <ewald@siyavula.com>
2013-08-04 20:03:25 +0200

    Initial dump of semi-broken TextbookHTML format files

---
+++ cnxmlplus/compile_all_to_html.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import os
+import subprocess
+
+#
+# Please enter the location of your local validator here.
+converterPath = "../cnxml-validator/tohtml.py"
+
+
+#
+#
+if __name__ == "__main__":
+    # find all cnxmlplus files in this folder:
+    cnxmlplusfiles = [f for f in os.listdir(os.curdir) if (f.endswith('.cnxmlplus')) and ('.' != f[0])]
+    cnxmlplusfiles.sort()
+    for cnxmlplusfile in cnxmlplusfiles:
+        with open('%s.html'%cnxmlplusfile, "w") as fout:
+            command = ['python', converterPath, cnxmlplusfile]
+            result = subprocess.call(command, stdout=fout)
+            print cnxmlplusfile,'.'*(60-len(cnxmlplusfile)), 
+            if result == 0:
+                print "ok"
+            else:
+                print "Conversion Failed"
+
+
