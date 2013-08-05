68d8380ce4ccf0f0a69c97f31937e50b25d35323
Ewald Zietsman <ewald@siyavula.com>
2013-08-05 23:50:21 +0200

    added converter script

---
+++ cnxmlplus/convert-all-html.sh
@@ -0,0 +1,8 @@
+python ../../cnxml-validator/tohtml.py --spec TextbookHTML-spec.xml 01-science-skills.cnxmlplus >  01-science-skills.cnxmlplus.html
+python ../../cnxml-validator/tohtml.py --spec TextbookHTML-spec.xml 02-momentum-and-impulse.cnxmlplus >  02-momentum-and-impulse.cnxmlplus.html
+python ../../cnxml-validator/tohtml.py --spec TextbookHTML-spec.xml 03-vertical-projectile-motion.cnxmlplus >  03-vertical-projectile-motion.cnxmlplus.html
+python ../../cnxml-validator/tohtml.py --spec TextbookHTML-spec.xml 05-work-energy-and-power.cnxmlplus >  05-work-energy-and-power.cnxmlplus.html
+python ../../cnxml-validator/tohtml.py --spec TextbookHTML-spec.xml 06-doppler-effect.cnxmlplus >  06-doppler-effect.cnxmlplus.html
+python ../../cnxml-validator/tohtml.py --spec TextbookHTML-spec.xml 10-electric-circuits.cnxmlplus >  10-electric-circuits.cnxmlplus.html
+python ../../cnxml-validator/tohtml.py --spec TextbookHTML-spec.xml 11-electrodynamics.cnxmlplus >  11-electrodynamics.cnxmlplus.html
+python ../../cnxml-validator/tohtml.py --spec TextbookHTML-spec.xml 12-optical-phenomena-and-properties-of-matter.cnxmlplus >  12-optical-phenomena-and-properties-of-matter.cnxmlplus.html
